51feefa64aaf4c041781a6f2174bf141c486b6d3
Chris Blackwell <funtech@gmail.com>
2021-12-05 00:18:50 -0800

    Rerder

--- Day05/vents.py
+++ Day05/vents.py
@@ -1,48 +1,3 @@
-def ventIntersections() -> None:
-    with open('input_test.txt') as f:
-        lines = f.read().splitlines()
-        coordPairs = []
-        for line in lines:
-            coordStrs = line.split(' -> ')
-            coordPair = (tuple(int(v) for v in coordStrs[0].split(',')), tuple(int(v) for v in coordStrs[1].split(',')))
-            coordPairs.append(coordPair)
-
-        # determine if line is horizontal or vertical or diagonal
-        # reject diagonals
-        # determine all points along line and increment count in map on the tuple
-        # count all entries > 1
-        grid = {}
-        for coordPair in coordPairs:
-            pair1 = coordPair[0]
-            pair2 = coordPair[1]
-            if pair1[0] == pair2[0]:
-                r = None
-                if pair1[1] > pair2[1]:
-                    r = range(pair2[1], pair1[1] + 1)
-                else:
-                    r = range(pair1[1], pair2[1] + 1)
-                for i in r:
-                    gridCoord = (pair1[0], i)
-                    grid[gridCoord] = grid.get(gridCoord, 0) + 1
-
-            elif pair1[1] == pair2[1]:
-                #horizontal line
-                r = None
-                if pair1[0] > pair2[0]:
-                    r = range(pair2[0], pair1[0] + 1)
-                else:
-                    r = range(pair1[0], pair2[0] + 1)
-                for i in r:
-                    gridCoord = (i, pair1[1])
-                    grid[gridCoord] = grid.get(gridCoord, 0) + 1
-        
-        count = 0
-        for value in grid.values():
-            if value > 1:
-                count += 1
-        
-        print(count)
-
 def ventIntersections2() -> None:
     with open('input.txt') as f:
         lines = f.read().splitlines()
@@ -103,7 +58,54 @@ def ventIntersections2() -> None:
             if value > 1:
                 count += 1
 
-        print(count)                
+        print(count)  
+        
+def ventIntersections() -> None:
+    with open('input_test.txt') as f:
+        lines = f.read().splitlines()
+        coordPairs = []
+        for line in lines:
+            coordStrs = line.split(' -> ')
+            coordPair = (tuple(int(v) for v in coordStrs[0].split(',')), tuple(int(v) for v in coordStrs[1].split(',')))
+            coordPairs.append(coordPair)
+
+        # determine if line is horizontal or vertical or diagonal
+        # reject diagonals
+        # determine all points along line and increment count in map on the tuple
+        # count all entries > 1
+        grid = {}
+        for coordPair in coordPairs:
+            pair1 = coordPair[0]
+            pair2 = coordPair[1]
+            if pair1[0] == pair2[0]:
+                r = None
+                if pair1[1] > pair2[1]:
+                    r = range(pair2[1], pair1[1] + 1)
+                else:
+                    r = range(pair1[1], pair2[1] + 1)
+                for i in r:
+                    gridCoord = (pair1[0], i)
+                    grid[gridCoord] = grid.get(gridCoord, 0) + 1
+
+            elif pair1[1] == pair2[1]:
+                #horizontal line
+                r = None
+                if pair1[0] > pair2[0]:
+                    r = range(pair2[0], pair1[0] + 1)
+                else:
+                    r = range(pair1[0], pair2[0] + 1)
+                for i in r:
+                    gridCoord = (i, pair1[1])
+                    grid[gridCoord] = grid.get(gridCoord, 0) + 1
+        
+        count = 0
+        for value in grid.values():
+            if value > 1:
+                count += 1
+        
+        print(count)
+
+              
 
 
 if __name__ == '__main__':
